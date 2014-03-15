fcfc67c0b3f28f566ae03994dde91ec475fd9b8a
Alekseylv <alekseylv@me.com>
2014-03-15 12:05:49 +0200

    refactored graph generation

--- src/test/scala/coloring/GraphGen.scala
+++ src/test/scala/coloring/GraphGen.scala
@@ -9,13 +9,13 @@ trait GraphGen {
 
   val arbInt = Gen.choose(10, 100)
 
-  val graphs: Gen[Graph] = for {
-    e <- Gen.choose(10, 100)
+  def randomGraph(from: Int, to: Int): Gen[Graph] = for {
+    e <- Gen.choose(from, to)
     v <- Gen.choose(e, e * (e - 1) / 2)
   } yield Graph.simple(e, v)
 
-  val smallGraphs: Gen[Graph] = for {
-    e <- Gen.choose(4, 20)
-    v <- Gen.choose(e, e * (e - 1) / 2)
-  } yield Graph.simple(e, v)
+  val graphs = randomGraph(10, 100)
+
+  val smallGraphs = randomGraph(4, 20)
+
 }
