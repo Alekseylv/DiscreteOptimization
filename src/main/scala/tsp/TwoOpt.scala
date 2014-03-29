27f4bf592f472785939c356c369fd18cc93a15e3
Alekseylv <alekseylv@me.com>
2014-03-29 16:26:53 +0200

    cleaning up garbage

--- src/main/scala/tsp/TwoOpt.scala
+++ src/main/scala/tsp/TwoOpt.scala
@@ -49,34 +49,3 @@ class TwoOpt(name: String, N: Int, data: Data) extends GreedySolve(name, N, data
     seq
   }
 }
-
-//val seq = super.solutionSequence.toArray
-//
-//var j = 2
-//var bestSwapIndex = -1
-//var minSwapLength = Double.MaxValue
-//var changeFromLength = 0.0
-//
-//val start = 0
-//var i = start
-//while (succ(i) != start) {
-//j = succ(succ(succ(i)))
-//if (succ(j) != start)
-//changeFromLength = length(seq(i), seq(succ(i))) + length(seq(pred(j)), seq(j))
-//bestSwapIndex = -1
-//minSwapLength = Double.MaxValue
-//while (succ(j) != start) {
-//val changeToLength = length(seq(i), seq(pred(j))) + length(seq(succ(i)), seq(j))
-//if (changeFromLength > changeToLength && minSwapLength > changeToLength) {
-//bestSwapIndex = j
-//minSwapLength = changeToLength
-//}
-//j =  succ(j)
-//}
-//if (bestSwapIndex != -1) {
-//swap(i, bestSwapIndex, seq)
-//}
-//i = succ(i)
-//}
-//
-//seq
