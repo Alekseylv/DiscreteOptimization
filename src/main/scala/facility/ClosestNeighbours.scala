7587d6c5bc09368cd16134ec353c2f9ca25f739a
Alekseylv <alekseylv@me.com>
2014-04-18 13:07:30 +0300

    changed model to consider only $size closest facilities for a customer

--- src/main/scala/facility/ClosestNeighbours.scala
+++ src/main/scala/facility/ClosestNeighbours.scala
@@ -10,50 +10,47 @@ import scala.collection.mutable
 trait ClosestNeighbours {
 
   def N: Int
-
   def M: Int
-
   def facilities: Array[Facility]
-
   def customers: Array[Customer]
 
   val size = 30
 
-  def getIndex(name: String): (Array[Set[Int]], Array[Set[Int]]) = {
+  def getIndex(name: String): (Array[Array[Int]], Array[Array[Int]]) = {
     val file = new File("cache/" + name)
 
     if (file.exists()) readAndParse(file)
     else createAndCompute(file)
   }
 
-  def createAndCompute(file: File): (Array[Set[Int]], Array[Set[Int]]) = {
+  def createAndCompute(file: File): (Array[Array[Int]], Array[Array[Int]]) = {
     file.getParentFile.mkdirs()
     file.createNewFile()
 
-    val warehouses = Array.fill(N)(new mutable.HashSet[Int]())
+    val warehouses = Array.fill(N)(new mutable.ArrayStack[Int]())
 
     val sorted = (0 to M - 1) map { x =>
-      val result = ((0 to N - 1).sortBy(p => length(facilities(p), customers(x))) take size).toSet
+      val result = ((0 to N - 1).sortBy(p => length(facilities(p), customers(x))) take size).toArray
       result foreach (p => warehouses(p) += x)
       result
     }
 
-    val first = warehouses map (_ mkString "") mkString "\n"
+    val first = warehouses map (_ mkString " ") mkString "\n"
     val second = sorted map (_ mkString " ") mkString "\n"
     val out = new java.io.PrintWriter(file)
 
     try {
-      out write first
+      out write first + "\n"
       out write second
     }
     finally out.close()
 
-    (warehouses map (_.toSet), sorted.toArray)
+    (warehouses map (_.toArray), sorted.toArray)
   }
 
-  def readAndParse(file: File): (Array[Set[Int]], Array[Set[Int]]) = {
+  def readAndParse(file: File): (Array[Array[Int]], Array[Array[Int]]) = {
 
-    val input = io.Source.fromFile(file).getLines().map(x => (x split " " map (_.toInt)).toSet).toList.splitAt(N)
+    val input = io.Source.fromFile(file).getLines().map(x => (x split " " map (_.toInt)).toArray).toList.splitAt(N)
 
     (input._1.toArray, input._2.toArray)
   }
--- src/main/scala/facility/MIP.scala
+++ src/main/scala/facility/MIP.scala
@@ -8,15 +8,31 @@ import Solver._
  */
 class MIP(val N: Int, val M: Int, val facilities: Array[Facility], val customers: Array[Customer]) extends ClosestNeighbours {
 
+  def findVariableIndex(arr: Array[Int], to: Int): Int = {
+    var i = 0
+    while (i < arr.length) {
+      if (arr(i) == to) return i
+      i += 1
+    }
+
+    throw new Error("Could not find index")
+  }
+
   def solution(name: String): Solution = {
 
     val solver = new MPSolver("IntegerProgrammingExample", MPSolver.getSolverEnum("CBC_MIXED_INTEGER_PROGRAMMING"))
     assert(solver != null)
 
+    val (wareIndex, custIndex) = getIndex(name)
+
+    assert(wareIndex.length == N)
+    assert(custIndex.length == M)
+
     val warehouses = solver.makeBoolVarArray(N)
-    val assignment = Array.fill(M)(solver.makeBoolVarArray(N))
+    val assignment = Array.fill(M)(solver.makeBoolVarArray(size))
 
     val inf = MPSolver.infinity()
+
 
     //minimize
     var i = 0
@@ -24,10 +40,11 @@ class MIP(val N: Int, val M: Int, val facilities: Array[Facility], val customers
       solver.objective().setCoefficient(warehouses(i), facilities(i)._1)
 
       val capacity = solver.makeConstraint(0, facilities(i)._2)
-      var j = 0
-      while (j < M) {
-        capacity.setCoefficient(assignment(j)(i), customers(j)._1)
-        j += 1
+
+      wareIndex(i) foreach { x =>
+        val index = findVariableIndex(custIndex(x), i)
+        capacity.setCoefficient(assignment(x)(index), customers(x)._1)
+
       }
 
       i += 1
@@ -38,13 +55,13 @@ class MIP(val N: Int, val M: Int, val facilities: Array[Facility], val customers
       var j = 0
       val global = solver.makeConstraint(1, 1)
 
-      while (j < N) {
+      while (j < size) {
         //set objective
-        solver.objective().setCoefficient(assignment(i)(j), length(facilities(j), customers(i)))
+        solver.objective().setCoefficient(assignment(i)(j), length(facilities(custIndex(i)(j)), customers(i)))
 
         //set constraints
         val c = solver.makeConstraint(0, inf)
-        c.setCoefficient(warehouses(j), 1)
+        c.setCoefficient(warehouses(custIndex(i)(j)), 1)
         c.setCoefficient(assignment(i)(j), -1)
 
         global.setCoefficient(assignment(i)(j), 1)
@@ -54,12 +71,14 @@ class MIP(val N: Int, val M: Int, val facilities: Array[Facility], val customers
       i += 1
     }
 
+    solver.setTimeLimit(1000 * 60 * 5)
     val status = solver.solve()
+
     if (status == MPSolver.ABNORMAL || status == MPSolver.UNBOUNDED || status == MPSolver.INFEASIBLE) throw new Error("Bad model")
 
-    //    println(solver.wallTime())
+    //        println(solver.wallTime())
     //   println(warehouses.map(_.solutionValue()).toList)
 
-    (solver.objective().value(), assignment map findTrue)
+    (solver.objective().value(), (0 to assignment.length - 1) map (fx => custIndex(fx)(findTrue(assignment(fx)))))
   }
 }
